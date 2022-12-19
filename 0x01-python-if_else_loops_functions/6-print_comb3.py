86702108916398fd00cac0e5caa8c15f3dc6c07f
root <emzzy>
2022-12-19 15:18:08 -0800

    number combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(0, 9):
         if i == 8 and j == 9:
             print('{}{}'.format(i, j))
             break
-        if i == j or j > i:
+        if i == j or i > j:
             continue
         print('{}{}, '.format(i, j), end='')
